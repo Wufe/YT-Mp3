0bd3071c49289e85fdfe78b049bb251ff06d18d2
Wufe <wufe92@gmail.com>
2015-05-20 23:00:23 +0200

    Test commit
    First test of download

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var Video = require( './lib/video' );
 
 var video = new Video();
-video.fetchVideo( "https://www.youtube.com/watch?v=sb9fHU4d3Ds" );
+video.fetchVideo( "https://www.youtube.com/watch?v=VPu9vdrP4LI&list=RDVPu9vdrP4LI#t=1696" );
