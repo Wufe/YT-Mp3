72330a9b8351a9827d2ca10afa83ea6ffd01fe8b
Wufe <wufe92@gmail.com>
2015-05-21 03:34:43 +0200

    Added download module
    Added download module from "https://github.com/Wufe/x-downloader"

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var Video = require( './lib/video' );
 var video = new Video();
 //video.fetchVideo( "https://www.youtube.com/watch?v=89DHFeSVxrg" );
 //video.fetchVideo( "https://www.youtube.com/watch?v=heJBwBUStXU&index=2&list=RDVPu9vdrP4LI" );
-video.fetchVideo( "https://www.youtube.com/watch?v=f-GdlZBaQTM", function(){
+video.fetchVideo( "https://www.youtube.com/watch?v=mmqv5SJH7Ko", function(){
 	video.getMP3();
 });
